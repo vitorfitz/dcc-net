87c7748d5e9b631c4f201fb57447a3079b443aa3
guilherme13c <guilhermesccaporali@protonmail.com>
2024-06-23 19:48:34 -0300

    .

--- dccnet-md5.py
+++ dccnet-md5.py
@@ -21,7 +21,7 @@ try:
         print("Maximum frame size is 65535")
         exit(-1)
 
-    frame, _ = write_frame(out_bytes, 0, True)
+    frame, check = write_frame(out_bytes, 0, False)
 
     attempts = 0
     ack = None
@@ -30,12 +30,14 @@ try:
         try:
             s.sendall(frame)
             ack = s.recv(HEADER_SIZE + SYNC_SIZE)
-            print("received ack " + str(len(ack)))
+            id_acked = int.from_bytes(ack[SYNC_SIZE+6:7])
+            print("received ack (id: " + str(id_acked) + ")")
         except socket.timeout:
             attempts += 1
 
     buf = ""
     run = True
+    id_ = 1
     while run:
         req = read_frame(s)
 
@@ -44,8 +46,13 @@ try:
 
         unpack, data = req
 
-        is_last = int(unpack[3] & 0x40) > 0
-        is_reset = int(unpack[3] & 0x20) > 0
+        is_ack = bool((unpack[3] & 0x80) >> 7)
+        is_last = bool((unpack[3] & 0x40) >> 6)
+        is_reset = bool((unpack[3] & 0x20) >> 5)
+
+        if is_ack:
+            id_ = not id_
+            continue
 
         if is_reset:
             print("Reseting")
@@ -61,7 +68,7 @@ try:
         for line in lines:
             if line.endswith('\n'):
                 check = calculate_checksum(line[:len(line)-1]).hexdigest()
-                frame, _ = write_frame(s, (str(check)+'\n').encode("ASCII"), is_last)
+                frame, _ = write_frame((str(check)+'\n').encode("ASCII"), id_, is_last)
 
                 attempts = 0
                 ack = None
@@ -70,7 +77,8 @@ try:
                     try:
                         s.sendall(frame)
                         ack = s.recv(HEADER_SIZE + SYNC_SIZE)
-                        print("received ack " + str(len(ack)))
+                        id_acked = int.from_bytes(ack[SYNC_SIZE + 6:7])
+                        print("received ack (id: " + str(id_acked) + ")")
                     except socket.timeout as e:
                         attempts += 1
             else:
--- utils.py
+++ utils.py
@@ -1,14 +1,14 @@
 import struct
 import socket
 
-HEADER_SIZE = 6
+HEADER_SIZE = 7
 SYNC_STRING = 2 * bytearray((0xDCC023C2).to_bytes(4, byteorder="big"))
 SYNC_SIZE = len(SYNC_STRING)
 MAX_FRAME_SIZE = 2 ** 16
 
 
 def get_ip_type(hostname, port_):
-	addr = socket.getaddrinfo(hostname, port_, 0, 0, socket.SOL_UDP)[0][4][0]
+	addr = socket.getaddrinfo(hostname, port_, 0, 0, socket.SOL_SOCKET)[0][4][0]
 	try:
 		socket.inet_pton(socket.AF_INET6, addr)
 		return socket.AF_INET6
@@ -48,10 +48,10 @@ def calculate_checksum(data):
 
 def write_frame(data, id_, is_last):
 	flags = (len(data) == 0) << 7 | is_last << 6
-	header = bytearray(struct.pack(">IIHHBB", 0xDCC023C2, 0xDCC023C2, 0, len(data), id_, flags))
+	header = bytearray(struct.pack(">IIHHHB", 0xDCC023C2, 0xDCC023C2, 0, len(data), id_, flags))
 	frame = header + data
 	# print(list(frame[8:]))
-	checksum = calculate_checksum(frame[SYNC_SIZE:])
+	checksum = calculate_checksum(frame)
 	frame[SYNC_SIZE:SYNC_SIZE + 2] = checksum.to_bytes(2, byteorder="big")
 	return frame, checksum
 
@@ -80,7 +80,7 @@ def read_frame(conn: socket.socket):
 		if not header or len(header) != HEADER_SIZE:
 			return None
 
-		unpacked = struct.unpack(">HHBB", header)
+		unpacked = struct.unpack(">HHHB", header)
 		expected_check = unpacked[0]
 		size = unpacked[1]
 
@@ -88,8 +88,8 @@ def read_frame(conn: socket.socket):
 		if len(data) != size:
 			continue
 
-		frame = header + data
-		frame[:2] = (0).to_bytes(2, byteorder="big")
+		frame = SYNC_STRING + header + data
+		frame[8:10] = (0).to_bytes(2, byteorder="big")
 		checksum = calculate_checksum(frame)
 		if checksum != expected_check:
 			continue
@@ -106,6 +106,6 @@ def frame_to_ascii(bytes_data: bytes):
 		line += '\n'
 
 	if str_data.endswith('\n'):
-		lines[-1] = lines[-1][:len(lines[-1])-1]
+		lines[-1] = lines[-1][:len(lines[-1]) - 1]
 
 	return lines
